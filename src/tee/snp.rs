c69157a4ab2ce263642758430ebb8ef5443b97f4
Tyler Fanelli <tfanelli@redhat.com>
2023-11-10 21:43:10 -0500

    snp: Remove SnpRequest
    Any information related to a challenge under SEV-SNP attesting VMs can
    be contained in the Request struct.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/tee/snp.rs
+++ src/tee/snp.rs
@@ -3,11 +3,6 @@ use serde::{Deserialize, Serialize};
 use crate::String;
 
 #[derive(Serialize, Deserialize)]
-pub struct SnpRequest {
-    pub workload_id: String,
-}
-
-#[derive(Serialize, Deserialize)]
 pub struct SnpAttestation {
     pub report: String,
     pub cert_chain: String,
